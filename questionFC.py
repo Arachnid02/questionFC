a61afe282f29abccd0fd63e63c0f55423a217ef6
Arachnid02 <jianwen.chen@uamaker.nyc>
2018-12-10 10:37:32 -0500

    Initalized variables and introduced program

---
+++ questionFC.py
@@ -0,0 +1,11 @@
+q1Check = bool(False)
+q1Answer = str("")
+q1 = str("""1. What is the technique called by which the genome of an organism can be
+split up into different sized molecules?
+    a. Electrolysis
+    b. Electrophrosis
+    c. Chromatography
+    d. None of the above""")
+
+print("""This is a one question science quiz. There will be four possible answers,
+and only one of them is correct.""")
