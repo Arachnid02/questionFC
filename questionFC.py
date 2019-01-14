c4ae5817cdde4acb7f9027974cdba3a63039f954
Frank Chen <jianwen.chen@uamaker.nyc>
2019-01-14 10:33:39 -0500

    Optimized if loop, added comments

--- questionFC.py
+++ questionFC.py
@@ -1,3 +1,4 @@
+#Initialize variables
 q1Check = bool(False)
 q1Answer = str("")
 q1 = str("""1. What is the technique called by which the genome of an organism can be
@@ -7,24 +8,32 @@ split up into different sized molecules?
     c. Chromatography
     d. None of the above\n""")
 
+#Gives the user information on what the program does, and how to properly use it
 print("""This is a one question science quiz. There will be four possible answers,
 and only one of them is correct. When selecting answers, enter the letter that
 precedes it.\n""")
 
+#Will try to get an acceptable answer from the user while q1Check is False
 while q1Check == False:
+        #Try to get an input of the specified type....
         try:
+                #Gets an input from the user and ensures that it's of type string
                 q1Answer = str(input(q1))
+                #Sets whatever the user input is to the lowercase version of it
                 q1Answer = q1Answer.lower()
+                #Checks to see if the input is the correct answer
                 if q1Answer == "b":
                         print("Got your answer choice!")
                         q1Check = True
+                #If it isn't check to see if it's an acceptable answer
+                elif "`" < q1Answer < "e":
+                        print("Got your answer choice!")
+                        q1Check = True
+                #If it isn't provide feedback
                 else:
-                        if "`" < q1Answer < "e":
-                                print("Got your answer choice!")
-                                q1Check = True
-                        else:
-                                print("""Please enter a letter that is between the
+                        print("""Please enter a letter that is between the
 letters A and D.\n""")
+        #If it doesn't skip directly here
         except ValueError:
                 print("""Please enter a letter. Select your answer choice by putting in
 the letter that precedes your answer choice.""")
